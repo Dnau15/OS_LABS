0bededa9dbf936ddf953d86e725dffe19289c24d
Dnau15 <106427673+Dnau15@users.noreply.github.com>
2022-09-12 19:38:51 +0300

    Update ex2.c

--- week02/ex2.c
+++ week02/ex2.c
@@ -1,20 +1,33 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
+
    char str[256];
+   
+   //reverse string
    char rev_str[256];
+   
+   
+   //reading input
    fgets(str, 256,stdin);
    int len = strlen(str);
+   
+   //index of the last character
    int last_char = 0;
+   
+   //looking for "." or "Enter key"
    for (int i = 0;i < len; i++){
-   	if ((str[i] == '.') || (str[i] == '\n')) {
-   	last_char = i-1;
-   	break;
-   	}
+   		if ((str[i] == '.') || (str[i] == '\n')) {
+   		//we should know index of the last character before "." or enter to create reverse string 
+   		last_char = i-1;
+   		break;
+   		}
    }
    
+   //creating the reverse string
    for (int i = 0;i <= last_char;i++){
-   	rev_str[i] = str[last_char - i];
+   		rev_str[i] = str[last_char - i];
    }
+   //printing the reverse string
    printf("%s", rev_str);
 }
