d36370105930334be51975796e50e06e7e1db6d3
Dnau15 <106427673+Dnau15@users.noreply.github.com>
2022-09-12 20:21:45 +0300

    Update ex5.c

--- week02/ex5.c
+++ week02/ex5.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
+
+void tribonacci(int n);
+
 int main(){
 	int n;
 	scanf("%d", &n);
+	printf("Tribonacci(4) = ");
+	tribonacci(4);
+	printf("Tribonacci(36) = ");
+	tribonacci(36);
+	tribonacci(n);
+}
+
+void tribonacci(int n){
 	int fst = 0;
 	int snd = 1;
 	int thd = 1;
 	long long current = 0;
-	for (int i = 3;i < n;i++){
+	for (int i = 3;i <= n;i++){
 		current = fst + snd + thd;
 		fst = snd;
 		snd = thd;
@@ -17,5 +28,5 @@ int main(){
 	if (n == 1) current = 1;
 	else 
 	if (n == 2) current = 1;
-	printf("%lld", current);
+	printf("%lld\n", current);
 }
