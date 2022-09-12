00e3e133fc3646d3e1500d8c4d7e5dc31c32fd97
Dnau15 <106427673+Dnau15@users.noreply.github.com>
2022-09-12 19:32:15 +0300

    Update ex1.c

--- week02/ex1.c
+++ week02/ex1.c
@@ -14,9 +14,9 @@ int main(){
    float_var = FLT_MAX;
    double_var = DBL_MAX;
    
-   printf("int size: %ld %d\n", sizeof(int), INT_MAX);
-   printf("unsigned short size: %ld %d\n", sizeof(unsigned short), USHRT_MAX);
-   printf("signed long int: %ld %lld\n", sizeof(signed long), LONG_MAX);
-   printf("float: %ld %f\n", sizeof(float), FLT_MAX);
-   printf("double: %ld %lf\n", sizeof(double), DBL_MAX);
+   printf("int size: %ld Value: %d\n", sizeof(int), INT_MAX);
+   printf("unsigned short size: %ld Value: %d\n", sizeof(unsigned short), USHRT_MAX);
+   printf("signed long int size: %ld Value: %lld\n", sizeof(signed long), LONG_MAX);
+   printf("float size: %ld Value: %f\n", sizeof(float), FLT_MAX);
+   printf("double size: %ld Value: %lf\n", sizeof(double), DBL_MAX);
 }
