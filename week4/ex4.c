cced8aa4e026fa7c8f203002fdc3f084c77fbdc5
Dnau15 <106427673+Dnau15@users.noreply.github.com>
2022-09-27 22:14:10 +0300

    Update ex4.c

--- week4/ex4.c
+++ week4/ex4.c
@@ -2,15 +2,22 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
-int main(int argc, char* argv[]){
-    char command[100];
-    printf("Enter command\n");
-    fgets(command, sizeof command, stdin);
-    pid_t id = fork();
-    if (id == 0){
-        system(command);
-        exit(0);
+#include<sys/wait.h>
+#include <stdbool.h>
+
+int main(){
+    while (true){
+        printf("> ");
+        char command[256];
+        fgets(command, sizeof command, stdin);
+        if ((command[0]=='e') && (command[1]=='x') && (command[2]=='i') && (command[3]=='t')){
+            return EXIT_SUCCESS;
+        }
+        pid_t id = fork();
+        if (id == 0){
+            system(command);
+            exit(0);
+        }
+        wait(NULL);
     }
-    
-   
 }
