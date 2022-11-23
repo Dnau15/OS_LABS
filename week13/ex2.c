bac4164b2c286a5967da28a3753e89a629810d7a
Dnau15 <106427673+Dnau15@users.noreply.github.com>
2022-11-23 18:42:40 +0300

    Update ex2.c

--- week13/ex2.c
+++ week13/ex2.c
@@ -1,17 +1,14 @@
-#include <sys/types.h>
 #include <dirent.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <sys/stat.h>
 
 int main() {
 	DIR *directory = opendir("/");
-	struct dirent *_dirent;
 
-	while ((_dirent = readdir(directory)) != NULL) {
-		printf("%s\n", _dirent->d_name);
+	struct dirent *dirent;
+
+	while ((dirent = readdir(directory)) != NULL) 
+	{
+		printf("%s\n", dirent->d_name);
 	}
-    
 	return 0;
 }
